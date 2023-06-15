d2f734fbe62d2efb1e39a6f70de49af1c92715b1
Alexander <sebalex1@outlook.com>
2023-06-14 22:51:18 -0500

    ACTUALIZACION

--- src/main/java/pe/edu/cibertec/ProyectoDAWII/controller/HomeController.java
+++ src/main/java/pe/edu/cibertec/ProyectoDAWII/controller/HomeController.java
@@ -1,13 +1,16 @@
 package pe.edu.cibertec.ProyectoDAWII.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 
     @GetMapping("/home")
     public String home(Model model)
     {
+
         return "home";
     }
 }
